42b9605cb4949b9830876a18c18b7e821d210b6a
Alejandro <janomalo_moli@hotmail.com>
2020-01-29 21:30:39 -0300

    Update rental.sql

--- rental.sql
+++ rental.sql
@@ -79,7 +79,7 @@ CREATE TABLE `usuarios` (
   `telefono` varchar(50) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `fecha_registro` datetime NOT NULL,
+  `fecha_registro` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `direccion` varchar(255) NOT NULL,
   `usuario` varchar(50) NOT NULL,
   `habilitado` tinyint(1) NOT NULL,
@@ -87,11 +87,11 @@ CREATE TABLE `usuarios` (
   UNIQUE KEY `uq_email` (`email`),
   KEY `fk_usuarios_rol` (`rol_id`),
   CONSTRAINT `fk_usuarios_rol` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 
 /*Data for the table `usuarios` */
 
-insert  into `usuarios`(`id`,`dni`,`rol_id`,`nombres`,`apellidos`,`telefono`,`email`,`password`,`fecha_registro`,`direccion`,`usuario`,`habilitado`) values (2,'34860732',1,'Ale','Molina','+542944335790','ale@ale.com','2be88ca4242c76e8253ac62474851065032d6833','2019-11-07 00:00:00','casa','alelala',1),(3,'34555222',2,'nonono','Molina1','55555','newemail@gmail.com','2be88ca4242c76e8253ac62474851065032d6833','0000-00-00 00:00:00','primero de mayo','aaaa',0),(6,'31555000',2,'Jorge','ref','154131351','jorge@gmail.com','45645646','0000-00-00 00:00:00','su casa','',0),(17,'124124124',2,'Alejandro','Molina','0192384847575','lala@lalala.com','2be88ca4242c76e8253ac62474851065032d6833','2019-11-27 00:00:00','jojojo 1234','',0),(18,'0192388485',2,'Ale1','bobo','02020202','bobo@gmail.com','c129b324aee662b04eccf68babba85851346dff9','2019-12-07 00:00:00','bobo 1234','bobo_baba',0),(19,'15151515',2,'probando2','molina','92919292','alemoli@hotmail.com','4bc08a047410fe93eb24c1d5b26524f9945d8be6','2020-01-20 00:00:00','primero','janomalo',1);
+insert  into `usuarios`(`id`,`dni`,`rol_id`,`nombres`,`apellidos`,`telefono`,`email`,`password`,`fecha_registro`,`direccion`,`usuario`,`habilitado`) values (2,'34860732',1,'Ale234','Molina','+542944335790','ale@ale.com','aversiseguarda','2019-11-07 00:00:00','casa','alelala',1),(3,'34555222',2,'nonono','Molina1','55555','newemail@gmail.com','2be88ca4242c76e8253ac62474851065032d6833','0000-00-00 00:00:00','primero de mayo','aaaa',0),(6,'31555000',2,'Jorge','ref','154131351','jorge@gmail.com','45645646','0000-00-00 00:00:00','su casa','',0),(20,'0192834',2,'JorgeL','Reffi','1029384','jor@gmail.com','123456','2020-01-29 21:22:01','primerodemayo','jorgel',1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
